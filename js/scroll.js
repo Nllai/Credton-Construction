a75207bc9a97cf803fccf66fe86e233143a9430a
LZ <lzhang74@binghamton.edu>
2020-10-22 01:04:03 -0700

    update styles

--- js/scroll.js
+++ js/scroll.js
@@ -28,15 +28,18 @@ window.onscroll = function(ev) {
     console.log('flag')
     if (!("scrolled" in document.getElementById("nav").classList)){
           document.getElementById("nav").classList.add("scrolled");
-          document.getElementById("nav").style.height = "10vh";
+          document.getElementById("nav").classList.remove("not-scrolled");
           document.getElementById("logo").style.padding = "0 0 0 4vw";
+          document.getElementById("logo").classList.add("scrolled-logo");
+
       }
   }
   else {
-      document.getElementById("nav").classList.remove("scrolled");
-      document.getElementById("nav").classList.add("not-scrolled");
-      document.getElementById("nav").style.height = "20vh";
-      document.getElementById("logo").style.padding = "1vw 0 1vw 4vw";
+    document.getElementById("nav").classList.remove("scrolled");
+    document.getElementById("nav").classList.add("not-scrolled");
+    document.getElementById("logo").style.padding = "1vw 0 1vw 4vw";
+    document.getElementById("logo").classList.remove("scrolled-logo");
+
   }
 };
 
